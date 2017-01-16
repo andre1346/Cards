c70502897f43b4d181a4c56b61a435df437e9779
andre1346 <andregirard5@hotmail.com>
2017-01-16 09:25:40 -0500

    version 2 jeux

--- lib/cards.ex
+++ lib/cards.ex
@@ -9,16 +9,20 @@ defmodule Cards do
 
 	def create_deck do
 	  values=["Ace", "Jack", "Quenn", "King", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
-	  suits=["Spades", "Clubs", "Hearts", "Diamonds"]
-
+	  suits=["Spades", "Clubs", "Hearts", "Diamond"]
+    
+        
 for suit<-suits, value <- values do
-	  	"#{value} of #{suit}"
+    List.duplicate("#{value} of #{suit}", 2)
+    List.flatten(["#{value} of #{suit}"])
 	 end
+     
+      end
 
-    end
   def shuffle(deck) do
   	Enum.shuffle(deck)
   end
+
   def contains?(deck, card) do
   	Enum.member?(deck, card)
   end	
@@ -27,12 +31,14 @@ for suit<-suits, value <- values do
   	"""
   def deal(deck, hand_size) do
   	Enum.split(deck, hand_size)
+  	
   	end
   	def save(deck, filename) do
   	  binary=:erlang.term_to_binary(deck)
   	  File.write(filename, binary)
   	end
   	def load (filename) do
+
   	  case File.read(filename) do
   	{:ok, binary} ->:erlang.binary_to_term binary
   	{:error, _reason}->"sa existe pas nono"
